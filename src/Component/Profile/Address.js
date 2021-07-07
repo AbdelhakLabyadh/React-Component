3bba4a555d2dc8218b0e680611f976328e01493c
Abdelhak Labyadh <labyadhabdelhak94@gmail.com>
2021-07-07 22:29:45 +0100

    react component checkpoint

---
+++ src/Component/Profile/Address.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const addressStyle = {
+    color: 'skyblue',
+    
+}
+
+const Address = () => {
+    return(
+       
+            <label style={addressStyle}>Ayoun Zerkine 2, Mareth, Gabes  6015</label>
+      
+    );
+};
+
+export default Address;
---
+++ src/Component/Profile/FullName.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const nameStyle = {
+    color: 'burlywood'
+}
+
+const Name = () => {
+    return(
+        <h1 style={nameStyle}>Abdelhak Labyadh</h1>
+    );
+};
+
+export default Name;
